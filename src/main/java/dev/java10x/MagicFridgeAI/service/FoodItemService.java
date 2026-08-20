b532fe86ccea1fe8949ca3823f786b3d30566871
CarfiCadu <carficadu@gmail.com>
2026-08-20 15:55:27 -0300

    feat: Criação do Repository e do Service do Projeto

--- src/main/java/dev/java10x/MagicFridgeAI/service/FoodItemService.java
+++ src/main/java/dev/java10x/MagicFridgeAI/service/FoodItemService.java
@@ -1,4 +1,41 @@
 package dev.java10x.MagicFridgeAI.service;
 
+import dev.java10x.MagicFridgeAI.model.FoodItem;
+import dev.java10x.MagicFridgeAI.repository.FoodItemRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class FoodItemService {
+
+    private final FoodItemRepository foodItemRepository;
+
+    public FoodItemService(FoodItemRepository foodItemRepository) {
+        this.foodItemRepository = foodItemRepository;
+    }
+
+    public FoodItem salvar(FoodItem foodItem) {
+        return foodItemRepository.save(foodItem);
+    }
+
+    public Optional<FoodItem> buscarPorId(Long id) {
+        return foodItemRepository.findById(id);
+    }
+
+    public List<FoodItem> listar() {
+        return foodItemRepository.findAll();
+    }
+
+    public FoodItem atualizar(FoodItem foodItem) {
+        return foodItemRepository.save(foodItem);
+    }
+
+    public Void excluir(Long id) {
+        foodItemRepository.deleteById(id);
+    }
+
+
 }
